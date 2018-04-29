e68a8af38a3447c211ff3d0071f0ee7b6e07d502
Nitisha Pandey <nitishapandey@Nitishas-MacBook-Pro.local>
2018-04-29 13:00:14 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,3 +1,25 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var message = 'I am user number 3'
+var password = '123abc!';
+
+bcrypt.genSalt(10,(err,salt) => {
+  bcrypt.hash(password, salt,(err,hash) =>{
+    console.log(hash);
+  });
+});
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded, decoded');
+var hashedPassword = '$2a$10$2/3cxLRiAp97g3K//cE/qOSCKgHxb0EYrgguTNL.twXXu1A9j/bhq'
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
